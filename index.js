e50046328d22f61fa663ef63689a8845a9e2a221
Narasimha <mudhenanarasimharao@gmail.com>
2025-06-12 13:38:33 +0530

    Complete RCG

---
+++ index.js
@@ -0,0 +1,13 @@
+const ar = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+ function change(){
+    let col = '#';
+    for(let i = 0;i<6;i++){
+            col += ar[Math.floor(Math.random()*16)];
+    }
+    document.getElementById("box").style.backgroundColor = col;
+    document.getElementById("text").innerHTML = col;
+    document.getElementById("text").style.color = col;
+    document.getElementById("bt").style.backgroundColor = col;
+}
+
+window.onload = change;
